6b05f1a46b5af8b5aee8e65256433426c573def7
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-08-02 02:27:26 +0300

    refactor initState code && navigate to home view

--- lib/feautures/splash/presetation/views/widgets/splash_screen_body.dart
+++ lib/feautures/splash/presetation/views/widgets/splash_screen_body.dart
@@ -1,6 +1,10 @@
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/feautures/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 import 'package:svg_flutter/svg.dart';
 
 import 'text_sliding_animation_builder.dart';
@@ -23,22 +27,18 @@ class _SplashScreenBodyState extends State<SplashScreenBody>
 
   @override
   void initState() {
-    animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 2));
-    slidingAnimationtext = Tween<Offset>(
-            begin: const Offset(
-              0,
-              10,
-            ),
-            end: Offset.zero)
-        .animate(animationController);
-    fadeAnimationlogoImage =
-        Tween<double>(begin: 0,end:1 )
-            .animate(animationController);
-
-    animationController.forward();
-
+    initAnimatedFunction();
+    navigateMethod();
     super.initState();
+  }
+
+  
+
+  @override
+  void dispose() {
+    animationController.dispose();
+
+    super.dispose();
   }
 
   @override
@@ -48,7 +48,6 @@ class _SplashScreenBodyState extends State<SplashScreenBody>
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         FadeTransition(
-        
           opacity: fadeAnimationlogoImage,
           child: SvgPicture.asset(
             AssetsData.logoPath,
@@ -62,5 +61,21 @@ class _SplashScreenBodyState extends State<SplashScreenBody>
         TextSlidingAnimationBuilder(slidingAnimation: slidingAnimationtext)
       ],
     );
+  }
+  void navigateMethod() {
+     Future.delayed(const Duration(seconds: 3), () {
+      Get.to(const HomeView(), transition: Transition.fade,duration: kNavigateDurayion);
+    });
+  }
+
+  void initAnimatedFunction() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    slidingAnimationtext =
+        Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero)
+            .animate(animationController);
+    fadeAnimationlogoImage =
+        Tween<double>(begin: 0, end: 1).animate(animationController);
+    animationController.forward();
   }
 }
