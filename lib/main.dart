d6946bfce18d07b231ef55bf73215ed9727cea09
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-08-01 13:53:56 +0300

    create spalsh view body

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import "package:bookly/feautures/splash/presetation/views/splash_view.dart";
+import "package:flutter/material.dart";
+import "package:get/get_navigation/src/root/get_material_app.dart";
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home:SplashView() ,
+    );
+  }
+}
+//1- separte your project to 2 folders
+     // 1- feauture folder => بتحط فيه صفحة او صفحتيه ليهم نفس ال => feature
+     //1- core folder=> بتحط فيه الحاجات المتشابهةبين كل ال => feuture=> عشان متكرش حاجة 
+//2- هنستخدم => get statemanagemet => عشان ندير بيها التنقل بين الصفحات
