a6c95cdd9d271f47f84b9f12afcedc0d6cf9031a
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-08-02 18:56:53 +0300

    best saller item part 1

--- lib/feautures/home/presentation/views/widgets/home_view_body.dart
+++ lib/feautures/home/presentation/views/widgets/home_view_body.dart
@@ -30,7 +30,47 @@ class HomeViewBody extends StatelessWidget {
             "Best Saller",
             style: Styles.titleMedium,
           ),
+          SizedBox(
+            height: 20,
+          ),
+          BestSallerListViewItem()
         ],
+      ),
+    );
+  }
+}
+
+class BestSallerListViewItem extends StatelessWidget {
+  const BestSallerListViewItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+      child: SizedBox(
+        height: 120,//مش مهتم بتكون ريسبونسيف لانها كدا كدا >ListView
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.7 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    image: const DecorationImage(
+                        image: NetworkImage(
+                          "https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg",
+                        ),
+                        fit: BoxFit.cover),
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+          ],
+        ),
       ),
     );
   }
