8337447986cc6887c3c7d41c0136bc60ad802c39
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-08-02 15:57:37 +0300

    add featured list item to home page

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,5 +1,6 @@
 class AssetsData {
   static const String logoPath = "assets/images/Logo.svg";
+  static const String ImageBook1 = "assets/images/Book 1 Hightligh.png";
 }
 //انت الكلاس ده هتحط فيه كل حاجة خاصة ب باثات الصور 
 //حطيتها في الفولدر ده عشان انت هتستخدمها في كل ال =>features
--- lib/feautures/home/presentation/views/widgets/home_view_body.dart
+++ lib/feautures/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,7 @@
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/feautures/home/presentation/views/widgets/custom_app_bar_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({
@@ -8,14 +10,35 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 24),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 20),
       child: Column(
         children: [
-          SizedBox(
+          const SizedBox(
             height: 40,
           ),
-          CustomAppBarWidget()
+          const CustomAppBarWidget(),
+          const SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height *
+                .25, //=> AspectRatio=> هترجعلك نسب العرض الى الطول على حسب هذه المساحة
+            child: AspectRatio(
+              aspectRatio: 2.6 /
+                  4, //بيظبطلك نسبة العرض الي الطول على حسب ال مساحة الموجود باخلها
+              child: Container(
+                decoration: BoxDecoration(
+                    image: const DecorationImage(
+                        image: AssetImage(
+                          AssetsData.ImageBook1,
+                        ),
+                        fit: BoxFit.cover),
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(20)),
+              ),
+            ),
+          )
         ],
       ),
     );
