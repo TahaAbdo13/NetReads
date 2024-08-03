dcb6fd1e6dca01dae46c734a7d15dd7909a8a406
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-08-03 18:08:49 +0300

    refactor best saller widget

---
+++ lib/feautures/home/presentation/views/widgets/best_saller_item_column_widget.dart
@@ -0,0 +1,62 @@
+import 'package:bookly/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class BestSallerItemColumnWidget extends StatelessWidget {
+  const BestSallerItemColumnWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          width: MediaQuery.of(context).size.width *
+              0.5, //عشان ياخد مساحة الشاشة المناسبة
+          child: const Text(
+            "Harry Potter and the Goblet of Fire",
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: Styles.textStyle20,
+          ),
+        ),
+        Text(
+          "J.K. Rowling",
+          style: Styles.textStyle14
+              .copyWith(color: Colors.white.withOpacity(.5)),
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 0.5,
+          child: Row(
+            children: [
+              const Text(
+                "19.99 €",
+                style: Styles.textStyle20,
+              ),
+              const Spacer(),
+              const Icon(
+                Icons.star,
+                color: Colors.orange,
+              ),
+              const SizedBox(
+                width: 6,
+              ),
+              Text(
+                "4.8",
+                style: Styles.textStyle16
+                    .copyWith(fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(width: 9),
+              Text(
+                "(2390)",
+                style: Styles.textStyle14
+                    .copyWith(color: Colors.white.withOpacity(.5)),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/feautures/home/presentation/views/widgets/best_saller_list_view_item.dart
+++ lib/feautures/home/presentation/views/widgets/best_saller_list_view_item.dart
@@ -1,5 +1,6 @@
-import 'package:bookly/core/utils/styles.dart';
+
 import 'package:flutter/material.dart';
+import 'best_saller_item_column_widget.dart';
 
 class BestSallerListViewItem extends StatelessWidget {
   const BestSallerListViewItem({
@@ -28,56 +29,7 @@ class BestSallerListViewItem extends StatelessWidget {
           const SizedBox(
             width: 30,
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width *
-                    0.5, //عشان ياخد مساحة الشاشة المناسبة
-                child: const Text(
-                  "Harry Potter and the Goblet of Fire",
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: Styles.textStyle20,
-                ),
-              ),
-              Text(
-                "J.K. Rowling",
-                style: Styles.textStyle14
-                    .copyWith(color: Colors.white.withOpacity(.5)),
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
-                child: Row(
-                  children: [
-                    const Text(
-                      "19.99 €",
-                      style: Styles.textStyle20,
-                    ),
-                    const Spacer(),
-                    const Icon(
-                      Icons.star,
-                      color: Colors.orange,
-                    ),
-                    const SizedBox(
-                      width: 6,
-                    ),
-                    Text(
-                      "4.8",
-                      style: Styles.textStyle16
-                          .copyWith(fontWeight: FontWeight.w500),
-                    ),
-                    const SizedBox(width: 9),
-                    Text(
-                      "(2390)",
-                      style: Styles.textStyle14
-                          .copyWith(color: Colors.white.withOpacity(.5)),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          )
+        const  BestSallerItemColumnWidget()
         ],
       ),
     );
