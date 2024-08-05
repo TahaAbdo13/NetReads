b503d591b3d130973cb4a6ec2433fa87b59842d7
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-08-05 17:41:23 +0300

    create home repo

---
+++ lib/feautures/home/data/repository/home_repo.dart
@@ -0,0 +1,9 @@
+abstract class HomeRepo {
+  // هنا بتوضح ال Home => بيحصل فيها اي
+  //Function without implementaion
+  //بتجبر الشخص اللي هيشتغل على الريبو دي ينفذ الفانكشن اللي جواها وكمان يعمل
+  //return ==> على حسب ما انت محدد
+  //انت كمان بتتحكم في اللي هتستقبله ال => method
+  fetchFeaturedBooks();
+  fetchBestSallerBooks();
+}
