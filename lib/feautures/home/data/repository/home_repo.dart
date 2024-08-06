57f0822bac5b13b8d59cb7361c2366b17c769417
TahaAbdalnaeem <tahaabdo212@gmail.com>
2024-08-06 17:42:47 +0300

    creat home repo method & add failure file

--- lib/feautures/home/data/repository/home_repo.dart
+++ lib/feautures/home/data/repository/home_repo.dart
@@ -1,9 +1,14 @@
+import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/feautures/home/data/models/book_model/book.mdel.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
   // هنا بتوضح ال Home => بيحصل فيها اي
   //Function without implementaion
   //بتجبر الشخص اللي هيشتغل على الريبو دي ينفذ الفانكشن اللي جواها وكمان يعمل
   //return ==> على حسب ما انت محدد
   //انت كمان بتتحكم في اللي هتستقبله ال => method
-  fetchFeaturedBooks();
-  fetchBestSallerBooks();
+ Future<Either<Failure,List<Book>>> fetchFeaturedBooks();
+  Future<Either<Failure,List<Book>>> fetchBestSallerBooks();
 }
+//في حالة الايرور انت متعرفش هترجع اي الافضل انك تعمل class هو ده اللي ترجعه وكل ايرور يقابلك تخليه ابن لهذا الكلاس
